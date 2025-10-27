275bfe3bf2c178d8e2f80c3666265d67caab8756
erfan <erfanbanaei110@gmail.com>
2025-10-27 17:11:24 +0330

    Refactor main.dart to include DeviceFrame for iPhone13 and adjust layout in MainApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:fhasier/pages/splash_page.dart';
+import 'package:device_frame/device_frame.dart';
 import 'package:flutter/material.dart';
+import 'package:fhasier/pages/splash_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,11 +13,21 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: "Mulish",
-      ),
-      home: const Scaffold(
-        body: SplashPage(),
+      home: Scaffold(
+        backgroundColor: Colors.white38,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: DeviceFrame(
+              device: Devices.ios.iPhone13,
+              screen: MaterialApp(
+                debugShowCheckedModeBanner: false,
+                theme: ThemeData(fontFamily: "Mulish"),
+                home: const SplashPage(),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
